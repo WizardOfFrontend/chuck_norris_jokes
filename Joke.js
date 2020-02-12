4390658c51aee0488ce9b8002db130a13b5741af
wizardofqa <wizardofqa@outlook.com>
2020-02-11 22:25:53 -0800

    validation and responsive design added.

--- Joke.js
+++ Joke.js
@@ -1,22 +1,28 @@
 document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(e) {
-    e.preventDefault()
-    const num = document.querySelector('input[type="number"]').value;
-    let output = "";
-    fetch(`http://api.icndb.com/jokes/random/${num}`)
-        .then(res => res.json())
-        .then(data => {
-            if (data.type === "success" && data.value.length > 0) {
+  e.preventDefault()
+  const num = document.querySelector('input[type="number"]').value;
 
+  fetch(`http://api.icndb.com/jokes/random/${num}`)
+    .then(res => res.json())
+    .then(data => {
+      let output = "";
 
-                data.value.forEach(eachJoke => {
-                    console.log(eachJoke.joke)
-                    output += `<li>Hi</li>`
-                });
-            }
-        })
-        .catch(err => console.log(err))
-    document.querySelector('.results').innerHTML = output;
+      if (data.type === "success") {
+        if (num > 0 && num < 21) {
+
+          data.value.forEach(eachJoke => {
+            console.log(eachJoke.joke)
+            output += `<li>${eachJoke.joke}</li>`
+          });
+        }
+        else {
+          output = '<h2 style="color:red;">You must enter the number between 1 and 20</h2>';
+        }
+        document.querySelector('.output').innerHTML = output;
+      }
+    })
+    .catch(err => console.log(err))
 }
 
